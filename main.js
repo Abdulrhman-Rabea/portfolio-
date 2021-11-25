45916bd8eebb0a6d301c9259898d55a1a52590c0
Abdulrhman_rabea <abdorabea181@gmail.com>
2021-11-26 00:09:05 +0200

    responsive with all screens

--- main.js
+++
@@ -1,6 +0,0 @@
-let observer = new IntersectionObserver(entries => {
-  // We will fill in the callback later...
-});
-
-// Tell the observer which elements to track
-observer.observe(document.querySelector('.about .sec3 .skills_levels .up'));
